17c6549c30d2e06b7732bb4c82894bb531460d7f
ayhan219 <ayhan-games@hotmail.com>
2024-09-17 13:32:30 +0300

    index.js changed

--- server/index.js
+++ server/index.js
@@ -15,19 +15,18 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log(`User connected : ${socket.id}`);
+  console.log(`User is connected : ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined the room: ${data}`);
   });
 
-  socket.on("send_message",(data)=>{
-    socket.to(data.room).emit("receive_message",data);
-    
-  })
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
   socket.on("disconnect", () => {
-    console.log(`user disconnected, ${socket.id}`);
+    console.log(`user is disconnected, ${socket.id}`);
   });
 });
 
